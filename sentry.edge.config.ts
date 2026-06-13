f30d4508e9fd490b3715312d7c369f8e2ddc8e8a
Sanjaee <afrizaahmad18@gmail.com>
2026-06-13 16:32:24 +0700

    feat: integrate Sentry error monitoring with instrumentation and example routes

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,7 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: "https://f0ac96785e7dc6a4b292670298e9d407@o4511556190404608.ingest.us.sentry.io/4511556192501760",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-  debug: false,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
